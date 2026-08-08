0e39087fe2e2decf2be660a21fab6cc2c6213a5b
sahelisarkar2002-dotcom <sahelisarkar2002@gmail.com>
2026-08-08 14:15:44 +0530

    NEW FILE

--- Nutritionist.py
+++ Nutritionist.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 #API Key
 GOOGLE_API_KEY = os.getenv["GOOGLE_API_KEY"]
-
+if not GOOGLE_API_KEY:
+ GOOGLE_API_KEY=st.secrets["GOOGLE_API_KEY"]
 client = genai.Client(api_key= GOOGLE_API_KEY)
 #Title
 st.title("🥗💪AI Diet Planner & Fitness Advisor")
